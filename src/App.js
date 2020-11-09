a07e44526ff829ea1b678335ddb71eb1965957a1
tannv_57 <NVantan94@gmail.com>
2020-11-09 11:15:20 +0900

    add structure files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Cards, Chart, CountryPicker } from './components';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Cards />
+                <CountryPicker />
+                <Chart />
+            </div>
+        )
+    }
+}
+
+export default App;
