72c22e7311508a865ddcc1423c75ad03035f9fe7
tannv_57 <NVantan94@gmail.com>
2020-11-10 13:05:46 +0900

    update Chart by country selected from country picker

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ class App extends React.Component {
     handleCountryChange = async (country) => {
         const fetchedData = await fetchData(country);
 
-        this.setState({ data: fetchedData })
+        this.setState({ data: fetchedData, country: country })
     }
 
     async componentDidMount() {
         const fetchedData = await fetchData();
 
-        this.setState({ data: fetchedData, country: country });
+        this.setState({ data: fetchedData});
     }
 
     render() {
@@ -29,7 +29,7 @@ class App extends React.Component {
             <div className={styles.container}>
                 <Cards data={data} />
                 <CountryPicker handleCountryChange={this.handleCountryChange} />
-                <Chart/>
+                <Chart data={data} country={country} />
             </div>
         )
     }
