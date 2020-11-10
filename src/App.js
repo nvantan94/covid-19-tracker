fdf95a786210b3df8c18db9d313be45b592fab0e
tannv_57 <NVantan94@gmail.com>
2020-11-10 12:49:49 +0900

    update Cards by selected country from Country Picker

--- src/App.js
+++ src/App.js
@@ -6,23 +6,30 @@ import { fetchData } from './api'
 
 class App extends React.Component {
     state = {
-        data: {}
+        data: {},
+        country: ''
+    }
+
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country);
+
+        this.setState({ data: fetchedData })
     }
 
     async componentDidMount() {
         const fetchedData = await fetchData();
 
-        this.setState({ data: fetchedData });
+        this.setState({ data: fetchedData, country: country });
     }
 
     render() {
-        const { data } = this.state;
+        const { data, country } = this.state;
 
         return (
             <div className={styles.container}>
                 <Cards data={data} />
-                <CountryPicker />
-                <Chart />
+                <CountryPicker handleCountryChange={this.handleCountryChange} />
+                <Chart/>
             </div>
         )
     }
