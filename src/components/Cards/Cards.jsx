8ee48a40fcd54e9c608ff7cfd4e1a6c898f7a8fa
tannv_57 <NVantan94@gmail.com>
2020-11-10 10:55:18 +0900

    custom styles for Card components

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -4,6 +4,8 @@ import CountUp from 'react-countup';
 
 import styles from './Cards.module.css';
 
+import cx from 'classnames';
+
 const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate }}) => {
     if (!confirmed)
         return 'Loading...';
@@ -11,7 +13,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate }}) => {
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
                         <Typography variant="h5">
@@ -27,7 +29,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate }}) => {
                     </CardContent>
                 </Grid>
 
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Recovered</Typography>
                         <Typography variant="h5">
@@ -43,7 +45,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate }}) => {
                     </CardContent>
                 </Grid>
 
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Deaths</Typography>
                         <Typography variant="h5">
